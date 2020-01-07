bb960c19b69b71f8646f6ad6f2fbfa89e99066df
leogtzr <leogutierrezramirez@gmail.com>
2020-01-07 10:56:58 -0700

    move code from main to external method

--- src/main/java/com/comedor/app/ComedorMenuApiApplication.java
+++ src/main/java/com/comedor/app/ComedorMenuApiApplication.java
@@ -20,71 +20,10 @@ public class ComedorMenuApiApplication {
 		// Creating a Workbook from an Excel file (.xls or .xlsx)
 		final Workbook workbook = WorkbookFactory.create(new File(args[0]));
 
-		final Sheet lastSheet = workbook.getSheetAt(0);
-		System.out.println(lastSheet.getSheetName());
+		final Sheet sheet = workbook.getSheetAt(0);
 
-		final int titleRowPosition = MenuUtils.titleTagPosition(lastSheet);
-		InputSheetFileRowIndexes.TITLE.adjust(titleRowPosition);
-
-		final Menu menu = new Menu();
-		final String title = lastSheet.getRow(InputSheetFileRowIndexes.TITLE.get()).getCell(0).getStringCellValue();
-		menu.setTitle(title);
-
-		// All the following code is iterable ... I mean ... I can use a single variable with a loop using X.values()
-		final Row breakFastRow = lastSheet.getRow(InputSheetFileRowIndexes.BREAKFAST.get());
-		final Row mainRow1 = lastSheet.getRow(InputSheetFileRowIndexes.MAIN1.get());
-		final Row mainRow2 = lastSheet.getRow(InputSheetFileRowIndexes.MAIN2.get());
-		final Row antojitosRow = lastSheet.getRow(InputSheetFileRowIndexes.ANTOJITO.get());
-		final Row side1Row = lastSheet.getRow(InputSheetFileRowIndexes.SIDE1.get());
-		final Row side2Row = lastSheet.getRow(InputSheetFileRowIndexes.SIDE2.get());
-		final Row soupOrCreamRow = lastSheet.getRow(InputSheetFileRowIndexes.SOUP_OR_CREAM.get());
-		final Row dessertRow = lastSheet.getRow(InputSheetFileRowIndexes.DESSERT.get());
-		final Row lightRow = lastSheet.getRow(InputSheetFileRowIndexes.LIGHT.get());
-
-		final Row saladsRow1 = lastSheet.getRow(InputSheetFileRowIndexes.SALADS.get());
-		final Row saladsRow2 = lastSheet.getRow(InputSheetFileRowIndexes.SALADS.get() + 1);
-		final Row saladsRow3 = lastSheet.getRow(InputSheetFileRowIndexes.SALADS.get() + 2);
-
-		final Map<Day, DayMeal> dayMealMenu = new HashMap<>();
-
-		for (final DayColumnIndexes i : DayColumnIndexes.values()) {
-			final List<Food> alternatives = new ArrayList<>();
-			final DayMeal dayMeal = new DayMeal();
-
-			final Optional<Food> breakFast = MenuUtils.extractMealInfoFromRow(breakFastRow, i, MealType.BREAKFAST);
-			final Optional<Food> opcion1 = MenuUtils.extractMealInfoFromRow(mainRow1, i, MealType.PRINCIPAL);
-			final Optional<Food> opcion2 = MenuUtils.extractMealInfoFromRow(mainRow2, i, MealType.PRINCIPAL);
-			final Optional<Food> antojito = MenuUtils.extractMealInfoFromRow(antojitosRow, i, MealType.ANTOJITO);
-			final Optional<Food> side1 = MenuUtils.extractMealInfoFromRow(side1Row, i, MealType.GUARNICION);
-			final Optional<Food> side2 = MenuUtils.extractMealInfoFromRow(side2Row, i, MealType.GUARNICION);
-			final Optional<Food> soupOrCream = MenuUtils.extractMealInfoFromRow(soupOrCreamRow, i, MealType.SOPA_O_CREMA);
-			final Optional<Food> dessert = MenuUtils.extractMealInfoFromRow(dessertRow, i, MealType.POSTRE);
-			final Optional<Food> light = MenuUtils.extractMealInfoFromRow(lightRow, i, MealType.LIGHT);
-
-			final Row[] saladRows = {saladsRow1, saladsRow2, saladsRow3};
-			final Optional<SaladBar> salads = MenuUtils.extractFromSaladsRow(saladRows, i);
-
-			opcion1.ifPresent(alternatives::add);
-			opcion2.ifPresent(alternatives::add);
-			antojito.ifPresent(alternatives::add);
-			side1.ifPresent(alternatives::add);
-			side2.ifPresent(alternatives::add);
-			soupOrCream.ifPresent(alternatives::add);
-			dessert.ifPresent(alternatives::add);
-			light.ifPresent(alternatives::add);
-
-			dayMeal.setLunchDinnerFoodAlternatives(alternatives);
-			breakFast.ifPresent(dayMeal::setBreakfast);
-
-			salads.ifPresentOrElse(sl -> dayMeal.setSalads(sl.getSalads()), () -> dayMeal.setSalads(new ArrayList<>()));
-
-			dayMealMenu.put(i.day(), dayMeal);
-		}
-
-		dayMealMenu.forEach((k, v) -> {
-			System.out.println(k);
-			System.out.println(v);
-		});
+		final Optional<Menu> menu = MenuUtils.extractMenuFromSheet(sheet);
+		System.out.println(menu);
 
 		workbook.close();
 
--- src/main/java/com/comedor/util/MenuUtils.java
+++ src/main/java/com/comedor/util/MenuUtils.java
@@ -1,16 +1,13 @@
 package com.comedor.util;
 
 import com.comedor.input.sheet.DayColumnIndexes;
-import com.comedor.menu.Food;
-import com.comedor.menu.MealType;
-import com.comedor.menu.SaladBar;
+import com.comedor.input.sheet.InputSheetFileRowIndexes;
+import com.comedor.menu.*;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
 
 public final class MenuUtils {
 
@@ -55,7 +52,7 @@ public final class MenuUtils {
         return Optional.empty();
     }
 
-    public static int titleTagPosition(final Sheet sheet) {
+    public static Optional<Integer> titleTagPosition(final Sheet sheet) {
         int pos = 4;
 
         for (int i = 1; i < Constants.MAX_VALUE_TO_FIND_TITLE; i++) {
@@ -71,7 +68,75 @@ public final class MenuUtils {
             }
         }
 
-        return pos;
+        return pos >= Constants.MAX_VALUE_TO_FIND_TITLE ? Optional.empty() : Optional.of(pos);
+    }
+
+    public static Optional<Menu> extractMenuFromSheet(final Sheet sheet) {
+
+        final Optional<Integer> titleRowPosition = MenuUtils.titleTagPosition(sheet);
+        if (titleRowPosition.isEmpty()) {
+            return Optional.empty();
+        }
+
+        titleRowPosition.ifPresent(pos -> InputSheetFileRowIndexes.TITLE.adjust(pos));
+
+        final Menu menu = new Menu();
+        final String title = sheet.getRow(InputSheetFileRowIndexes.TITLE.get()).getCell(0).getStringCellValue();
+        menu.setTitle(title);
+
+        final Row breakFastRow = sheet.getRow(InputSheetFileRowIndexes.BREAKFAST.get());
+        final Row mainRow1 = sheet.getRow(InputSheetFileRowIndexes.MAIN1.get());
+        final Row mainRow2 = sheet.getRow(InputSheetFileRowIndexes.MAIN2.get());
+        final Row antojitosRow = sheet.getRow(InputSheetFileRowIndexes.ANTOJITO.get());
+        final Row side1Row = sheet.getRow(InputSheetFileRowIndexes.SIDE1.get());
+        final Row side2Row = sheet.getRow(InputSheetFileRowIndexes.SIDE2.get());
+        final Row soupOrCreamRow = sheet.getRow(InputSheetFileRowIndexes.SOUP_OR_CREAM.get());
+        final Row dessertRow = sheet.getRow(InputSheetFileRowIndexes.DESSERT.get());
+        final Row lightRow = sheet.getRow(InputSheetFileRowIndexes.LIGHT.get());
+
+        final Row saladsRow1 = sheet.getRow(InputSheetFileRowIndexes.SALADS.get());
+        final Row saladsRow2 = sheet.getRow(InputSheetFileRowIndexes.SALADS.get() + 1);
+        final Row saladsRow3 = sheet.getRow(InputSheetFileRowIndexes.SALADS.get() + 2);
+
+        final Map<Day, DayMeal> dayMealMenu = new HashMap<>();
+
+        for (final DayColumnIndexes i : DayColumnIndexes.values()) {
+            final List<Food> alternatives = new ArrayList<>();
+            final DayMeal dayMeal = new DayMeal();
+
+            final Optional<Food> breakFast = MenuUtils.extractMealInfoFromRow(breakFastRow, i, MealType.BREAKFAST);
+            final Optional<Food> option1 = MenuUtils.extractMealInfoFromRow(mainRow1, i, MealType.PRINCIPAL);
+            final Optional<Food> option2 = MenuUtils.extractMealInfoFromRow(mainRow2, i, MealType.PRINCIPAL);
+            final Optional<Food> antojito = MenuUtils.extractMealInfoFromRow(antojitosRow, i, MealType.ANTOJITO);
+            final Optional<Food> side1 = MenuUtils.extractMealInfoFromRow(side1Row, i, MealType.GUARNICION);
+            final Optional<Food> side2 = MenuUtils.extractMealInfoFromRow(side2Row, i, MealType.GUARNICION);
+            final Optional<Food> soupOrCream = MenuUtils.extractMealInfoFromRow(soupOrCreamRow, i, MealType.SOPA_O_CREMA);
+            final Optional<Food> dessert = MenuUtils.extractMealInfoFromRow(dessertRow, i, MealType.POSTRE);
+            final Optional<Food> light = MenuUtils.extractMealInfoFromRow(lightRow, i, MealType.LIGHT);
+
+            final Row[] saladRows = {saladsRow1, saladsRow2, saladsRow3};
+            final Optional<SaladBar> salads = MenuUtils.extractFromSaladsRow(saladRows, i);
+
+            option1.ifPresent(alternatives::add);
+            option2.ifPresent(alternatives::add);
+            antojito.ifPresent(alternatives::add);
+            side1.ifPresent(alternatives::add);
+            side2.ifPresent(alternatives::add);
+            soupOrCream.ifPresent(alternatives::add);
+            dessert.ifPresent(alternatives::add);
+            light.ifPresent(alternatives::add);
+
+            dayMeal.setLunchDinnerFoodAlternatives(alternatives);
+            breakFast.ifPresent(dayMeal::setBreakfast);
+
+            salads.ifPresentOrElse(sl -> dayMeal.setSalads(sl.getSalads()), () -> dayMeal.setSalads(new ArrayList<>()));
+
+            dayMealMenu.put(i.day(), dayMeal);
+        }
+
+        menu.setMenu(dayMealMenu);
+
+        return Optional.of(menu);
     }
 
 }
